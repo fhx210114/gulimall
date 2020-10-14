eeb6a28af6c5eab39f5e51710b499087cdbe74df
Kite <55333@qq.com>
2020-10-14 15:46:37 +0800

    新增第三方微服务 用于文件上传

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,14 +1,19 @@
 package com.atguigu.gulimall.product;
 
-import com.atguigu.gulimall.product.entity.BrandEntity;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.gulimall.product.service.BrandService;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
@@ -16,6 +21,30 @@ class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+//    @Autowired
+//    OSSClient ossClient;
+
+
+
+
+    @Test
+    public  void update() throws FileNotFoundException {
+            // Endpoint以杭州为例，其它Region请按实际情况填写。
+         String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        String accessKeyId = "LTAI4G9j9eehHMmFKB1ciJu9";
+        String accessKeySecret = "TrSy4RYMGAIKWLh8U80oSLT4OUj6T0";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+         //上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\86185\\Pictures\\Camera Roll\\RUbeN8NHFbwRkkH4-AuKbQ.jpg");
+        ossClient.putObject("gulimall-kites", "Raa", inputStream);
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传成功");
+    }
     @Test
     void contextLoads() {
 
@@ -37,6 +66,8 @@ class GulimallProductApplicationTests {
 //                    System.out.println(item);
 //                }
 //        );
+
+
 
 
     }
